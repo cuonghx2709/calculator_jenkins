366161eb047f6e377852a97ff72900c89c55b6a8
Cuonghx-0882 <hoang.xuan.cuong@sun-asterisk.com>
2020-08-26 14:38:57 +0700

    Add sum logic, controller and unit test

--- src/main/java/com/cuonghx/cacultor/demo/CalculatorController.java
+++ src/main/java/com/cuonghx/cacultor/demo/CalculatorController.java
@@ -11,8 +11,7 @@ class CalculatorController {
     private Calculator calculator;
 
     @RequestMapping("/sum")
-    String sum(@RequestParam("a") Integer a,
-    @RequestParam("b") Integer b) {
+    String sum(@RequestParam("a") Integer a, @RequestParam("b") Integer b) {
         return String.valueOf(calculator.sum(a, b));
     }
 }
---
+++ src/test/java/com/cuonghx/cacultor/demo/CalculatorTest.java
@@ -0,0 +1,13 @@
+package com.cuonghx.cacultor.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    private Calculator calculator = new Calculator();
+    @Test
+    public void testSum() {
+        assertEquals(5, calculator.sum(2, 3));
+    }
+}
